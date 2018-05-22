b0c769f7af6a1a7e6f194d4d568a4fa0e2a47fbd
Marco Willi <will5448@umn.edu>
2018-05-22 01:48:19 -0500

    prepare image compression script

--- setup/misc_unix_cmds.sh
+++ setup/misc_unix_cmds.sh
@@ -1,2 +1,5 @@
 # count number of lines in files
 wc -l file.txt
+
+# git reset to master
+git reset --hard origin/master
---
+++ zooniverse_exports/aggregation.sh
@@ -0,0 +1,4 @@
+python3 -m /home/packerc/shared/machine_learning/will5448/code/aggregation-for-caesar/bin/extract_panoptes_csv.py /home/packerc/shared/machine_learning/will5448/data/zooniverse_exports/4715_classifications.csv \
+/home/packerc/shared/machine_learning/will5448/data/zooniverse_exports/4715_workflows.csv \
+1 \
+-o /home/packerc/shared/machine_learning/will5448/data/zooniverse_exports/4715_extraction.csv
