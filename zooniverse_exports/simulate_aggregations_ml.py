923f1320fa825b8589a1456c24d89a28ca0c0615
Marco Willi <will5448@umn.edu>
2018-06-20 14:02:45 -0500

    fix bug

--- zooniverse_exports/simulate_aggregations_ml.py
+++ zooniverse_exports/simulate_aggregations_ml.py
@@ -106,9 +106,9 @@ if __name__ == '__main__':
 
             if rule == 'all':
                 res = self.aggregateSpeciesLabels(max_annos=None, ret=True)
-            elif rule = 'max_2':
+            elif rule == 'max_2':
                 res = self.subject.aggregateSpeciesLabels(2, ret=True)
-            elif rule = 'max_5':
+            elif rule == 'max_5':
                 res = self.subject.aggregateSpeciesLabels(5, ret=True)
             elif rule in ['empty_first_two_95conf', 'empty_first_two_90conf',
                           'empty_first_95conf', 'species_first_two_95conf']:
