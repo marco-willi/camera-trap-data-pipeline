65fa1593682fed3432eb86a0e2a9740c603ee867
Marco Willi <will5448@umn.edu>
2018-06-28 14:48:51 -0500

    create manifest and prediction files

---
+++ zooniverse_uploads/create_predict_file_from_manifest.py
@@ -0,0 +1,51 @@
+""" Create a Prediction File from a Manifest for a Model to
+    Generate Predictions
+"""
+import csv
+import json
+import os
+import argparse
+
+# For Testing
+# args = dict()
+# args['manifest'] = "/home/packerc/shared/zooniverse/Manifests/RUA/RUA_S1_manifest.json"
+# args['prediction_file'] = "/home/packerc/shared/machine_learning/data/info_files/RUA/RUA_S1/RUA_S1_new.csv"
+
+
+if __name__ == "__main__":
+
+    # Parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-manifest", type=str, required=True)
+    parser.add_argument("-prediction_file", type=str, required=True)
+
+    args = vars(parser.parse_args())
+
+    for k, v in args.items():
+        print("Argument %s: %s" % (k, v))
+
+    # Check Inputs
+    prediction_file_path = os.path.split(args['prediction_file'])[0]
+    if not os.path.isdir(prediction_file_path):
+        raise FileNotFoundError("Path to store predictions: %s is not a \
+                                directory" %
+                                prediction_file_path)
+
+    if not os.path.exists(args['manifest']):
+        raise FileNotFoundError("manifest: %s not found" %
+                                args['manifest'])
+
+    # Read the Manifest
+    with open(args['manifest'], 'r') as f:
+        manifest = json.load(f)
+
+    print("Found %s capture events in %s" %
+          (len(manifest.keys()), args['manifest']))
+
+    # Extract all image paths and store them to disk
+    with open(args['prediction_file'], "w", newline='') as outs:
+        csv_writer = csv.writer(outs, delimiter=',')
+        print("Writing file to %s" % args['prediction_file'])
+        for capture_id, mani_data in manifest.values():
+            for image in mani_data['images']['original_images']:
+                csv_writer.writerow(image)
--- zooniverse_uploads/generate_manifest.py
+++ zooniverse_uploads/generate_manifest.py
@@ -6,14 +6,14 @@ import argparse
 from collections import OrderedDict
 
 # For Testing
-args = dict()
-args['cleaned_captures_csv'] = "/home/packerc/shared/zooniverse/season_captures/RUA/cleaned/RUA_S1_cleaned.csv"
-args['input_image_dir'] = "/home/packerc/shared/zooniverse/ToUpload/RUA/Compressed_S1/"
-args['output_manifest_dir'] = "/home/packerc/shared/zooniverse/Manifests/RUA/"
-args['manifest_id'] = 'RUA_S1'
-args['csv_quotechar'] = '"'
-args['attribution'] = 'University of Minnesota Lion Center + SnapshotSafari + Ruaha Carnivore Project + Tanzania + Ruaha National Park'
-args['license'] =  'SnapshotSafar + Ruaha Carnivore Project'
+# args = dict()
+# args['cleaned_captures_csv'] = "/home/packerc/shared/season_captures/RUA/cleaned/RUA_S1_cleaned.csv"
+# args['compressed_image_dir'] = "/home/packerc/shared/zooniverse/ToUpload/RUA_will5448/RUA_S1_Compressed/"
+# args['output_manifest_dir'] = "/home/packerc/shared/zooniverse/Manifests/RUA/"
+# args['manifest_prefix'] = 'RUA_S1'
+# args['csv_quotechar'] = '"'
+# args['attribution'] = 'University of Minnesota Lion Center + SnapshotSafari + Ruaha Carnivore Project + Tanzania + Ruaha National Park'
+# args['license'] =  'SnapshotSafar + Ruaha Carnivore Project'
 
 
 if __name__ == "__main__":
@@ -21,9 +21,9 @@ if __name__ == "__main__":
     # Parse command line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("-cleaned_captures_csv", type=str, required=True)
-    parser.add_argument("-input_image_dir", type=str, required=True)
+    parser.add_argument("-compressed_image_dir", type=str, required=True)
     parser.add_argument("-output_manifest_dir", type=str, required=True)
-    parser.add_argument("-manifest_id", type=str, required=True)
+    parser.add_argument("-manifest_prefix", type=str, required=True)
     parser.add_argument("-attribution", type=str, required=True)
     parser.add_argument("-license", type=str, required=True)
     parser.add_argument("-csv_quotechar", type=str, default='"')
@@ -38,18 +38,18 @@ if __name__ == "__main__":
         raise FileNotFoundError("output_manifest_dir: %s is not a directory" %
                                 args['output_manifest_dir'])
 
-    if not os.path.isdir(args['input_image_dir']):
-        raise FileNotFoundError("input_image_dir: %s is not a directory" %
-                                args['input_image_dir'])
+    if not os.path.isdir(args['compressed_image_dir']):
+        raise FileNotFoundError("compressed_image_dir: %s is not a directory" %
+                                args['compressed_image_dir'])
 
     if not os.path.exists(args['cleaned_captures_csv']):
         raise FileNotFoundError("cleaned_captures_csv: %s not found" %
                                 args['cleaned_captures_csv'])
 
-    if '.' in args['manifest_id']:
-        raise ValueError("manifest_name must not contain a dot")
+    if '.' in args['manifest_prefix']:
+        raise ValueError("manifest_prefix must not contain a dot")
 
-    manifest_file_name = args['manifest_id'] + '_manifest.json'
+    manifest_file_name = args['manifest_prefix'] + '_manifest.json'
     manifest_path = os.path.join(args['output_manifest_dir'],
                                  manifest_file_name)
 
@@ -72,7 +72,7 @@ if __name__ == "__main__":
           (len(cleaned_captures), args['cleaned_captures_csv']))
 
     # Find all processed images
-    images = set(os.listdir(args['output_image_dir']))
+    images_on_disk = set(os.listdir(args['compressed_image_dir']))
 
     # Create a Manifest which is a dictionary / json like structure
     # containing all information about the dataset
@@ -86,52 +86,67 @@ if __name__ == "__main__":
         capture = row[name_to_id_mapper['capture']]
         image_no = row[name_to_id_mapper['image']]
         image_name = row[name_to_id_mapper['imname']]
+        image_path = row[name_to_id_mapper['path']]
         invalid = row[name_to_id_mapper['invalid']]
-
-        exclude_invalid_codes = (0, 3)
-
-        # Skip image if invalid code
-        if invalid in exclude_invalid_codes:
+        valid_codes = ('0', '3')
+        # Skip image if not in valid codes
+        if invalid not in valid_codes:
             continue
-
         # Skip if image is not on disk
-        if image_name not in images:
+        if image_name not in images_on_disk:
             continue
-
         # unique capture id
         capture_id = '#'.join([season, site, roll, capture])
-
         # Create a new entry in the manifest
         if capture_id not in manifest:
-
-            # generate metadata
-            metadata = {
+            # generate metadata for uploading to Zooniverse
+            upload_metadata = {
                 '#site': site,
                 '#roll': roll,
                 '#capture': capture,
                 'attribution': args['attribution'],
                 'license': args['license']
             }
-
-            # generate upload status
-            status = {
+            # store additional information
+            info = {
                 'uploaded': False
             }
-
-            images = []
-
+            images = {
+                'original_images': [],
+                'compressed_images': []
+            }
             manifest[capture_id] = {
-                'metadata': metadata, 'status': status, 'images': images}
-
-        # Add current image
-        current_data = manifest[capture_id]
-        image_disk_path = os.path.join(args['output_image_dir'], image_name)
-        current_data['images'].append(image_disk_path)
+                'upload_metadata': upload_metadata, 'info': info,
+                'images': images}
+        # Add image information
+        image_disk_path = os.path.join(
+            args['compressed_image_dir'], image_name)
+        manifest[capture_id]['images']['compressed_images'].append(
+            image_disk_path)
+        manifest[capture_id]['images']['original_images'].append(
+            image_path)
 
     # Export Manifest
     with open(manifest_path, 'w') as outfile:
+        first_row = True
         for _id, values in manifest.items():
-            json.dump({_id: values}, outfile)
-            outfile.write('\n')
-    # with open(manifest_path, 'w') as fp:
-    #     json.dump(manifest, fp, indent=0)
+            if first_row:
+                outfile.write('{')
+                outfile.write('"%s":' % _id)
+                json.dump(values, outfile)
+                first_row = False
+            else:
+                outfile.write(',\n')
+                outfile.write('"%s":' % _id)
+                json.dump(values, outfile)
+        outfile.write('}')
+
+# with open(manifest_path, 'r') as f:
+#     mani = json.load(f)
+
+# Read the Manifest
+# mani = OrderedDict()
+# with open(manifest_path, 'r') as f:
+#     for line in f:
+#         dict_entry = json.loads(line)
+#         mani.update(dict_entry)
