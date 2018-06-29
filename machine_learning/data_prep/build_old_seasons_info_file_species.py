70717cfc6ae11218ddc44f1c938f195861386104
Marco Willi <will5448@umn.edu>
2018-06-28 20:52:17 -0500

    add main clause

--- machine_learning/data_prep/build_old_seasons_info_file_species.py
+++ machine_learning/data_prep/build_old_seasons_info_file_species.py
@@ -3,27 +3,29 @@ import os
 import csv
 import random
 
-# Parameters
-output_file = '/home/packerc/shared/machine_learning/data/info_files/SER/species_prev.csv'
-class_paths = '/home/packerc/shared/machine_learning/data/info_files/SER/'
-file_names = ['S1_6.csv', 'S9_species.csv', 'S8.csv', 'S7_species.csv']
-# 14292 in species train for S11
-sampling_size = 9*1500
+if __name__ == '__main__':
 
-all_records = list()
-for file in file_names:
-    file_path = os.path.join(class_paths, file)
-    with open(file_path, 'r') as f:
-        csv_reader = csv.reader(f, delimiter=',')
-        for line in csv_reader:
-            # remove capture id
-            line.pop(1)
-            all_records.append(line)
-# sample
-final_sample = random.sample(all_records, sampling_size)
+    # Parameters
+    output_file = '/home/packerc/shared/machine_learning/data/info_files/SER/species_prev.csv'
+    class_paths = '/home/packerc/shared/machine_learning/data/info_files/SER/'
+    file_names = ['S1_6.csv', 'S9_species.csv', 'S8.csv', 'S7_species.csv']
+    # 14292 in species train for S11
+    sampling_size = 9*1500
 
-# Write to disk
-with open(output_file, 'w') as f:
-    csv_writer = csv.writer(f, delimiter=',')
-    for line in final_sample:
-        csv_writer.writerow(line)
+    all_records = list()
+    for file in file_names:
+        file_path = os.path.join(class_paths, file)
+        with open(file_path, 'r') as f:
+            csv_reader = csv.reader(f, delimiter=',')
+            for line in csv_reader:
+                # remove capture id
+                line.pop(1)
+                all_records.append(line)
+    # sample
+    final_sample = random.sample(all_records, sampling_size)
+
+    # Write to disk
+    with open(output_file, 'w') as f:
+        csv_writer = csv.writer(f, delimiter=',')
+        for line in final_sample:
+            csv_writer.writerow(line)
