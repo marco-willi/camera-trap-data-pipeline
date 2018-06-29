fafb883033381798338539385fbd3ea694a40316
Marco Willi <will5448@umn.edu>
2018-06-28 21:55:12 -0500

    change format

--- machine_learning/data_prep/mix_old_info_species_blank.py
+++ machine_learning/data_prep/mix_old_info_species_blank.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
                     class_flag = 0
                 else:
                     class_flag = 1
-                class_records.append([path, class_flag])
+                class_records.append(path, class_flag)
         class_sample = random.sample(class_records, sampling_size)
         all_records.append(class_sample)
 
