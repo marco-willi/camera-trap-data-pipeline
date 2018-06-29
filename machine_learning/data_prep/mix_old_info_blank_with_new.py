8b1de0134a2691903ae2ac5ca2c3a6480783fd19
Marco Willi <will5448@umn.edu>
2018-06-28 22:39:41 -0500

    prep fine tuning SER model on new data

---
+++ machine_learning/data_prep/mix_old_info_blank_with_new.py
@@ -0,0 +1,25 @@
+""" Concatenate Empty Prev data with new Data
+"""
+import random
+
+if __name__ == '__main__':
+
+    # Parameters
+    output_file = '/home/packerc/shared/machine_learning/data/info_files/SER/SER_S11/SER_S11_empty_prev_train.csv'
+    prev_file = '/home/packerc/shared/machine_learning/data/info_files/SER/blanks_species_prev.csv'
+    current_file = '/home/packerc/shared/machine_learning/data/info_files/SER/SER_S11/SER_S11_empty_train.csv'
+    files = [prev_file, current_file]
+
+    all_records = list()
+    for file_path in files:
+        with open(file_path, 'r') as f:
+            for line in f:
+                all_records.append(line)
+
+    # randomly permuate
+    random.shuffle(all_records)
+
+    # Write to disk
+    with open(output_file, 'w') as f:
+        for r in all_records:
+            f.write(r)
