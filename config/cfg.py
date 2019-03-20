3c0f4a8bbee0a977847d8679b211d62f2e0932c8
Marco Willi <will5448@umn.edu>
2019-03-20 12:55:27 -0500

    change due to old yaml version

--- config/cfg.py
+++ config/cfg.py
@@ -4,11 +4,11 @@ import os
 
 working_dir = os.path.abspath(os.path.dirname(__file__))
 cfg_path = os.path.join(working_dir, "../config/cfg.yaml")
-cfg = yaml.load(open(cfg_path, 'r'), Loader=yaml.FullLoader)
+cfg = yaml.load(open(cfg_path, 'r')) # Loader=yaml.FullLoader)
 
 # Read Mappings (if available)
 try:
     ml_mappings_path = os.path.join(working_dir, "../config/ml_mappings.yaml")
-    ml_mappings = yaml.load(open(ml_mappings_path, 'r'),  Loader=yaml.FullLoader)
+    ml_mappings = yaml.load(open(ml_mappings_path, 'r')) #,  Loader=yaml.FullLoader)
 except:
     ml_mappings = None
