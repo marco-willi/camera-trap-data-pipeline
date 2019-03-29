546da5438794dd9dadffb3e14444a16f33235a43
Marco Willi <will5448@umn.edu>
2019-03-29 01:59:40 -0500

    handle legacy and new cases wrt date/time extraction

--- reporting/create_zooniverse_report.py
+++ reporting/create_zooniverse_report.py
@@ -60,6 +60,64 @@ def deduplicate_captures(df_aggregated):
     return df_aggregated
 
 
+def _get_datetime_obj(date_str):
+    """ Get DateTime object from a str with unknown format """
+    try:
+        time_obj = datetime.strptime(date_str, '%Y:%m:%d %H:%M:%S')
+    except ValueError:
+        try:
+            time_obj = datetime.strptime(
+                date_str,
+                '%Y-%m-%d %H:%M:%S')
+        except ValueError:
+            try:
+                time_obj = datetime.strptime(
+                    date_str,
+                    '%Y-%m-%d %H:%M:%SZ')
+            except ValueError:
+                logger.debug(
+                    "could not convert {} to datetime".format(date_str))
+    return time_obj
+
+
+def _extract_datetime(image_record):
+    """ Extract Date and Time from Image Record in Cleaned Season File
+        - handle different cases
+    """
+    res = {'date': '', 'time': ''}
+    # handle legacy case
+    if 'timestamp' in image_record:
+        try:
+            time_obj = _get_datetime_obj(image_record['timestamp'])
+            res['date'] = time_obj.strftime("%Y-%m-%d")
+            res['time'] = time_obj.strftime("%H:%M:%S")
+        except:
+            pass
+    # handle standard case
+    elif all([x in image_record for x in ['date', 'time']]):
+        res['date'] = image_record['date']
+        res['time'] = image_record['time']
+    # handle datetime only case
+    elif 'datetime' in image_record:
+        try:
+            time_obj = _get_datetime_obj(image_record['datetime'])
+            res['date'] = time_obj.strftime("%Y-%m-%d")
+            res['time'] = time_obj.strftime("%H:%M:%S")
+        except:
+            pass
+    # handle file creation date only
+    elif 'file_creation_date' in image_record:
+        try:
+            time_obj = _get_datetime_obj(image_record['file_creation_date'])
+            res['date'] = time_obj.strftime("%Y-%m-%d")
+            res['time'] = time_obj.strftime("%H:%M:%S")
+        except:
+            pass
+    if (res['date'] == '') or (res['time'] == ''):
+        raise ValueError("could not extract date/time from image record")
+    return res
+
+
 def create_season_dict(season_data_df):
     """ Create Dict of Season Data """
     season_dict = OrderedDict()
@@ -74,29 +132,21 @@ def create_season_dict(season_data_df):
                 image_record['roll'],
                 image_record['capture']])
         if capture_id not in season_dict:
-            timestamp = image_record['timestamp']
-            # deal with different timeformats in different capture files
             try:
-                time_obj = datetime.strptime(timestamp, '%Y:%m:%d %H:%M:%S')
-            except ValueError:
-                try:
-                    time_obj = datetime.strptime(
-                        timestamp,
-                        '%Y-%m-%d %H:%M:%S')
-                except ValueError:
-                    time_obj = datetime.strptime(
-                        timestamp,
-                        '%Y-%m-%d %H:%M:%SZ')
-            date = time_obj.strftime("%Y-%m-%d")
-            time = time_obj.strftime("%H:%M:%S")
+                date_time = _extract_datetime(image_record)
+            except:
+                date_time = {'date': '', 'time': ''}
+                logger.info("Failed to extract date/time for {}".format(
+                    capture_id))
+                logger.debug("Full record: {}".format(image_record))
             season_dict[capture_id] = {
                 'capture_id': capture_id,
                 'season': image_record['season'],
                 'site': image_record['site'],
                 'roll': image_record['roll'],
                 'capture': image_record['capture'],
-                'capture_date_local': date,
-                'capture_time_local': time}
+                'capture_date_local': date_time['date'],
+                'capture_time_local': date_time['time']}
     return season_dict
 
 
