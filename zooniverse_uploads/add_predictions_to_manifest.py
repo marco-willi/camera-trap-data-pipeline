835c775c5c6a938123fdf36252b47be210180f99
Marco Willi <will5448@umn.edu>
2019-04-09 00:26:48 -0500

    fix parameter to include all species scores

--- zooniverse_uploads/add_predictions_to_manifest.py
+++ zooniverse_uploads/add_predictions_to_manifest.py
@@ -107,7 +107,7 @@ if __name__ == "__main__":
         if capture_id in preds_species:
             flat_species = flatten_ml_species_preds(
                 preds_species[capture_id],
-                only_top=args['add_all_species_scores'])
+                only_top=(not args['add_all_species_scores']))
             flat_species = {
                 '#{}'.format(k): v for k, v in flat_species.items()}
             meta_data.update(flat_species)
