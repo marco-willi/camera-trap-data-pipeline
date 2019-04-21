e8074a39bf31d3152c7639edc7b42c7d5d08ede3
Marco Willi <will5448@umn.edu>
2019-04-21 02:30:11 -0500

    update create action list

--- pre_processing/create_action_list.py
+++ pre_processing/create_action_list.py
@@ -170,7 +170,7 @@ if __name__ == '__main__':
                   'image_name']
     info_cols = [
      'days_to_last_image_taken', 'days_to_next_image_taken',
-     'datetime', 'date', 'time',	'datetime_file_creation',
+     'datetime', 'datetime_file_creation',
      'image_path', 'image_path_rel']
 
     first_cols += action_cols
